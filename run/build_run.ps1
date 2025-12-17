15fbc3bb1f167d4193b6a21056d64b945440a2bb
Peter Meißner <retep.meissner@gmail.com>
2025-12-17 10:11:20 +0100

    minor dev build improvement

--- run/build_run.ps1
+++ run/build_run.ps1
@@ -6,3 +6,6 @@ $DATE = $(Get-Date -Format "yyyy-MM-dd_HH:mm:ss")
 
 
 go build -ldflags "-X main.Version=$VERSION -X main.BuildTime='$DATE' -X main.GitCommit='$GIT_COMMIT'" -o "log_stat-dev.exe" ..\src\ 
+
+Write-Host "Build completed: log_stat-dev.exe"
+.\log_stat-dev.exe -version
